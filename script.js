08b6d4b849276960fc5c334bcfa215c4a47a5cb9
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2025-02-18 11:56:51 +0000

    add alert

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 class basket {
     
-    basket(a,b){
+    basket(a,b,c ){
 
         this.product=a
         this.qty=b
+        this.price=c
     }
     addItem (a, b) {
         product=a
         qty=b
     }
     getCost(){
+     return this.qty*this.price;
      
     }
 }
 
-const basket = new basket.addItem('Apple', 23)
-
+const basket = new basket.addItem('Apple', 2, 2.5);
+let p=basket.getCost();
+alert(`The Price of the product is ${p}`)
 
