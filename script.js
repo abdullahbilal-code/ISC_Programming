a041d4198bef0474c02e055befc79fed5353fba9
Abdullah Bilal <79226329+abdullahbilal-code@users.noreply.github.com>
2025-02-18 11:05:31 +0000

    18 Feb start

---
+++ script.js
@@ -0,0 +1,19 @@
+class basket {
+    
+    basket(a,b){
+
+        this.product=a
+        this.qty=b
+    }
+    addItem (a, b) {
+        product=a
+        qty=b
+    }
+    getCost(){
+     
+    }
+}
+
+const basket = new basket.addItem('Apple', 23)
+
+
